83e72db3211ba4b565a08920fdb158ab1dfa9202
Patrick Figel <pfigel@greenpeace.org>
2022-02-08 15:56:47 +0100

    GP-25238 Add optional postprocessor config for mail accounts

--- Civi/Mailutils/Processor.php
+++ Civi/Mailutils/Processor.php
@@ -32,9 +32,29 @@ class Processor {
   public function process() {
     $meta = new Message($this->mail, $this->activityId, $this->mailSettings);
     $meta->process();
+    $caseId = NULL;
     if (!empty($this->mailSettings['support_case_category_id'])) {
       $case = new SupportCase($this->activityId, $this->mailSettings['support_case_category_id']);
-      $case->process();
+      $caseId = $case->process();
+    }
+    if (!empty($this->mailSettings['advanced_config']['postprocessor']['sqltasks'])) {
+      foreach ($this->mailSettings['advanced_config']['postprocessor']['sqltasks'] as $taskId) {
+        try {
+          civicrm_api3('Sqltask', 'execute', [
+            'id'          => $taskId,
+            'input_val'   => json_encode([
+              'type'                     => 'postprocessor',
+              'activity_id'              => $this->activityId,
+              'support_case_category_id' => $this->mailSettings['support_case_category_id'],
+              'case_id'                  => $caseId,
+            ]),
+            'log_to_file' => 1,
+            'check_permissions' => FALSE,
+          ]);
+        } catch (\CiviCRM_API3_Exception $e) {
+          Civi::log('mailutils')->error("Error executing post-processor SQL Task {$taskId}: {$e->getMessage()}");
+        }
+      }
     }
   }
 
